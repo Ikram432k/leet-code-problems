868efbfdf482fa1097404c76f45fd772fbb45fda
Ikram432k <ikram432k@gmail.com>
2025-10-23 20:03:27 +0530

    Reverse a String by modifying the input array in-place

---
+++ reverseString344.js
@@ -0,0 +1,26 @@
+// Reverse String
+// Write a function that reverses a string. The input string is given as an array of characters s.
+
+// You must do this by modifying the input array in-place with O(1) extra memory.
+
+ 
+
+// Example 1:
+
+// Input: s = ["h","e","l","l","o"]
+// Output: ["o","l","l","e","h"]
+// Example 2:
+
+// Input: s = ["H","a","n","n","a","h"]
+// Output: ["h","a","n","n","a","H"]
+
+var reverseString = function(s) {
+    for(let i = 0;i<Math.floor(s.length/2);i++){
+        let temp = s[i];
+        s[i] = s[(s.length-1)-i];
+        s[(s.length-1)-i] = temp;
+    }
+    return s;
+};
+
+console.log(reverseString(["h","e","l","l","o"]))
