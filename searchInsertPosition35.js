d9bd926cf90d5f836d520442cc58de28a9126403
Ikram432k <ikram432k@gmail.com>
2025-10-30 22:24:02 +0530

    return the index if the target element is found

---
+++ searchInsertPosition35.js
@@ -0,0 +1,34 @@
+// //35. Search Insert Position
+
+// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,3,5,6], target = 5
+// Output: 2
+// Example 2:
+
+// Input: nums = [1,3,5,6], target = 2
+// Output: 1
+// Example 3:
+
+// Input: nums = [1,3,5,6], target = 7
+// Output: 4
+
+var searchInsert = function(nums, target) {
+    let left = 0;
+    let right = nums.length-1 ;
+    while(left <= right){
+        let midEle = Math.floor((left+right)/2);
+        if(nums[midEle] === target) return midEle;
+        else if(nums[midEle] < target) left = midEle+1;
+        else right  = midEle-1 
+    }
+    return left;
+};
+
+console.log(searchInsert([1,3,5,6],5))
